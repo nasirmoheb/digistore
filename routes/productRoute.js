9ff751f2b5404b70d7e5aca505fd22bb11f8dd19
Nasir ahmad Moheb <nasir.moheb111@gmail.com>
2021-02-03 21:07:42 +0430

    update product route

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,6 +3,15 @@ const productController = require('../controllers/productController');
 
 const router = express.Router();
 
-router.route('/').post(productController.createProduct);
+router
+  .route('/')
+  .get(productController.getAllProduct)
+  .post(productController.createProduct);
+
+router
+  .route('/:id')
+  .get(productController.getProduct)
+  .patch(productController.updateProduct)
+  .delete(productController.deleteProduct);
 
 module.exports = router;
