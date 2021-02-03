71c0ee84834764c6473fd831ebb4f33e464a7c33
Nasir ahmad Moheb <nasir.moheb111@gmail.com>
2021-02-03 21:07:06 +0430

    adding create,update,delete and get Product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,18 +1,63 @@
 const Product = require('./../models/producModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
-exports.createProduct = async (req, res, next) => {
-  try {
-    const newProduct = await Product.create(req.body);
+exports.createProduct = catchAsync(async (req, res, next) => {
+  const newProduct = await Product.create(req.body);
 
-    res.json({
-      status: 'success',
-      data: newProduct
-    });
-  } catch (err) {
-    console.log(err);
+  res.status(201).json({
+    status: 'success',
+    data: newProduct
+  });
+});
+
+exports.getAllProduct = catchAsync(async (req, res, next) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    status: 'success',
+    length: products.length,
+    data: products
+  });
+});
+
+exports.getProduct = catchAsync(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new AppError('No document found with that ID', 404));
   }
-};
 
-exports.getProduct = async (req, res, next) => {
-  
-};
+  res.status(200).json({
+    status: 'success',
+    data: product
+  });
+});
+
+exports.updateProduct = catchAsync(async (req, res, next) => {
+  const newProduct = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  if (!newProduct) {
+    return next(new AppError('No document found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: newProduct
+  });
+});
+
+exports.deleteProduct = catchAsync(async (req, res, next) => {
+  const product = await Product.findByIdAndDelete(req.params.id);
+
+  if (!product) {
+    return next(new AppError('No document found with that ID', 404));
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
