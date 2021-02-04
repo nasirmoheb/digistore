2f6afe707a3554b85a2cf0b5111460f0b625c877
Nasir ahmad Moheb <nasir.moheb111@gmail.com>
2021-02-04 15:11:17 +0430

    Api Feature moved to his own class.

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,60 +1,17 @@
 const Product = require('./../models/producModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const APIFeature = require('./../utils/apiFeature');
 
 exports.getAllProduct = catchAsync(async (req, res, next) => {
-  //Build query
-  //1)Filtering
-  let queryObj = { ...req.query };
-  //exclude fields that are not in database
-  const excludedFields = ['page', 'sort', 'limit', 'fields'];
-  excludedFields.forEach(e => delete queryObj[e]);
-
-  //2)Advanced Filtering
-  let queryStr = JSON.stringify(queryObj);
-  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-  queryObj = JSON.parse(queryStr);
-
-  let query = Product.find(queryObj);
-
-  //3)sorting
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(',').join(' ');
-    query = query.sort(sortBy);
-  } else {
-    //default sort by time of creation
-    query = query.sort('-createdAt');
-  }
-
-  //4)Feild Limiting
-  if (req.query.fields) {
-    const fields = req.query.fields.split(',').join(' ');
-    query = query.select(fields);
-    console.log(fields);
-  } else {
-    //default don't show (__v)
-    query = query.select('-__v');
-  }
-
-  //5)Paginating
-  if (req.query.page && req.query.limit) {
-    //page
-    const page = req.query.page * 1;
-    //limit item per page
-    const limit = req.query.limit * 1;
-    //calculate skip
-    const skip = page * limit - limit;
-    //paginating
-    query = query.skip(skip).limit(limit);
-  } else {
-    //default limit 100 item in page
-    query = query.skip(0).limit(100);
-  }
-
-  //6)
+  const features = new APIFeature(Product.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
 
   //Execute query
-  const products = await query;
+  const products = await features.query;
 
   //send response
   res.status(200).json({
