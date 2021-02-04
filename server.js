2f88c43d7b66a8e519a0e08ba5086c7b234c7188
Nasir ahmad Moheb <nasir.moheb111@gmail.com>
2021-02-04 15:13:46 +0430

    uncaughtException and unhandledRejection updated to work correctly.

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const server = require('./app');
 
 //Envoirment var config
 dotenv.config({ path: './config.env' });
+
+//handling uncaughtException
+process.on('uncaughtException', err => {
+  console.log(err);
+  console.log('(💥uncaughtException) Shutting down System .........*******');
+  process.exit(1);
+});
+
+//importing the app file
+const app = require('./app');
 
 //Database connection
 const DB = process.env.DATABASE_LOCAL;
@@ -17,21 +26,14 @@ mongoose
 
 const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
+//handling uncaughtException
 process.on('unhandledRejection', err => {
   console.log(err);
   console.log('(💥unhandledRejection) Shutting down System .........*******');
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
-process.on('uncaughtException', err => {
-  console.log(err);
-  console.log('(💥uncaughtException) Shutting down System .........*******');
   server.close(() => {
     process.exit(1);
   });
