740bf966d5d71ea06c901e2af6317b498f8e5f60
Nasir ahmad Moheb <nasir.moheb111@gmail.com>
2021-02-12 15:22:57 +0430

    nested route for creating review on product and showing all reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -3,6 +3,12 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
 exports.createReview = catchAsync(async (req, res, next) => {
+  //Nested Routes
+  //if user don't specify the product id get it from params
+  if (!req.body.product) req.body.product = req.params.productId;
+  //if user don't' specify the user id get it from current user
+  if (!req.body.user) req.body.user = req.user.id;
+
   const review = await Review.create(req.body);
 
   res.status(200).json({
@@ -12,7 +18,9 @@ exports.createReview = catchAsync(async (req, res, next) => {
 });
 
 exports.getAllReview = catchAsync(async (req, res, next) => {
-  const review = await Review.find().populate('user');
+  const filter = {};
+  if (req.params.productId) filter.product = req.params.productId;
+  const review = await Review.find(filter).populate('user');
 
   res.status(200).json({
     status: 'success',
